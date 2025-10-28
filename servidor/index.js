d93f614923c0740e0d8c42330229c43c0c960c60
Joshua Lopez <demetrioo.12b@gmail.com>
2025-10-28 20:57:06 -0300

    hola

--- servidor/index.js
+++ servidor/index.js
@@ -1,18 +1,34 @@
+
 const express = require('express');
 const mysql = require('mysql');
 
+const morgan = require ("morgan");
+
 const app = express();
+app.use (cors());
+app.use (express.json())
+app.use (morgan("dev"));
+
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
   database: 'tp',
-  user: 'root'  
+  user: 'root'
+  
 })
+
+//app.post ("/api/asistencias", (req,res) =>
+  
+
 
 // esto conecta a la base de datos
 connection.connect(err => {
   if (err) throw err;
   console.log('DB conectada');  
 });
+
+
+
 
 app.listen(3000, () => console.log('Server andando...'));
